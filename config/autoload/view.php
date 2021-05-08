38fb799f7b8ea7f7d025692e4bcdf56facbee819
liangguifeng <1476982312@qq.com>
2021-05-08 12:06:30 +0800

    修复文件路径

--- config/autoload/view.php
+++ config/autoload/view.php
@@ -25,13 +25,12 @@ return [
         // 缓存文件存放位置
         'cache_path' => BASE_PATH . '/runtime/view/',
     ],
-
-    # 自定义组件注册
+    // 自定义组件注册
     'components' => [
         // 'alert' => \App\View\Components\Alert::class
     ],
-    # 视图命名空间 (主要用于扩展包中)
+    // 视图命名空间 (主要用于扩展包中)
     'namespaces' => [
-        'web' => BASE_PATH . '/resources/views/web',
+        'web' => BASE_PATH . '/resources/views',
     ],
 ];
